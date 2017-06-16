44fcb8052e7ddc27f226128b41d7d5a1ae7e9821
Cristian Tala Sanchez <yomismo@cristiantala.cl>
2017-06-16 17:24:23 +0200

    Impuesto Unico

--- src/detalle.php
+++ src/detalle.php
@@ -2,8 +2,7 @@
 
 namespace ctala\Facto;
 
-class detalle
-{
+class detalle {
 
     /**
      * @var float $cantidad
@@ -41,6 +40,16 @@ class detalle
     protected $descuentorecargo_porcentaje = null;
 
     /**
+     * @var int $impuesto_codigo
+     */
+    protected $impuesto_codigo = null;
+
+    /**
+     * @var float $impuesto_valorunitario
+     */
+    protected $impuesto_valorunitario = null;
+
+    /**
      * @param float $cantidad
      * @param string $unidad
      * @param string $glosa
@@ -49,141 +58,146 @@ class detalle
      * @param int $descuentorecargo_monto
      * @param float $descuentorecargo_porcentaje
      */
-    public function __construct($cantidad, $unidad, $glosa, $monto_unitario, $exento_afecto, $descuentorecargo_monto, $descuentorecargo_porcentaje)
-    {
-      $this->cantidad = $cantidad;
-      $this->unidad = $unidad;
-      $this->glosa = $glosa;
-      $this->monto_unitario = $monto_unitario;
-      $this->exento_afecto = $exento_afecto;
-      $this->descuentorecargo_monto = $descuentorecargo_monto;
-      $this->descuentorecargo_porcentaje = $descuentorecargo_porcentaje;
+    public function __construct($cantidad, $unidad, $glosa, $monto_unitario, $exento_afecto, $descuentorecargo_monto, $descuentorecargo_porcentaje, $impuesto_codigo = null, $impuesto_valorunitario = null) {
+        $this->cantidad = $cantidad;
+        $this->unidad = $unidad;
+        $this->glosa = $glosa;
+        $this->monto_unitario = $monto_unitario;
+        $this->exento_afecto = $exento_afecto;
+        $this->descuentorecargo_monto = $descuentorecargo_monto;
+        $this->descuentorecargo_porcentaje = $descuentorecargo_porcentaje;
+        $this->impuesto_codigo = $impuesto_codigo;
+        $this->impuesto_valorunitario = $impuesto_valorunitario;
     }
 
     /**
      * @return float
      */
-    public function getCantidad()
-    {
-      return $this->cantidad;
+    public function getCantidad() {
+        return $this->cantidad;
     }
 
     /**
      * @param float $cantidad
      * @return \WSDL\Facto\detalle
      */
-    public function setCantidad($cantidad)
-    {
-      $this->cantidad = $cantidad;
-      return $this;
+    public function setCantidad($cantidad) {
+        $this->cantidad = $cantidad;
+        return $this;
     }
 
     /**
      * @return string
      */
-    public function getUnidad()
-    {
-      return $this->unidad;
+    public function getUnidad() {
+        return $this->unidad;
     }
 
     /**
      * @param string $unidad
      * @return \WSDL\Facto\detalle
      */
-    public function setUnidad($unidad)
-    {
-      $this->unidad = $unidad;
-      return $this;
+    public function setUnidad($unidad) {
+        $this->unidad = $unidad;
+        return $this;
     }
 
     /**
      * @return string
      */
-    public function getGlosa()
-    {
-      return $this->glosa;
+    public function getGlosa() {
+        return $this->glosa;
     }
 
     /**
      * @param string $glosa
      * @return \WSDL\Facto\detalle
      */
-    public function setGlosa($glosa)
-    {
-      $this->glosa = $glosa;
-      return $this;
+    public function setGlosa($glosa) {
+        $this->glosa = $glosa;
+        return $this;
     }
 
     /**
      * @return float
      */
-    public function getMonto_unitario()
-    {
-      return $this->monto_unitario;
+    public function getMonto_unitario() {
+        return $this->monto_unitario;
     }
 
     /**
      * @param float $monto_unitario
      * @return \WSDL\Facto\detalle
      */
-    public function setMonto_unitario($monto_unitario)
-    {
-      $this->monto_unitario = $monto_unitario;
-      return $this;
+    public function setMonto_unitario($monto_unitario) {
+        $this->monto_unitario = $monto_unitario;
+        return $this;
     }
 
     /**
      * @return boolean
      */
-    public function getExento_afecto()
-    {
-      return $this->exento_afecto;
+    public function getExento_afecto() {
+        return $this->exento_afecto;
     }
 
     /**
      * @param boolean $exento_afecto
      * @return \WSDL\Facto\detalle
      */
-    public function setExento_afecto($exento_afecto)
-    {
-      $this->exento_afecto = $exento_afecto;
-      return $this;
+    public function setExento_afecto($exento_afecto) {
+        $this->exento_afecto = $exento_afecto;
+        return $this;
     }
 
     /**
      * @return int
      */
-    public function getDescuentorecargo_monto()
-    {
-      return $this->descuentorecargo_monto;
+    public function getDescuentorecargo_monto() {
+        return $this->descuentorecargo_monto;
     }
 
     /**
      * @param int $descuentorecargo_monto
      * @return \WSDL\Facto\detalle
      */
-    public function setDescuentorecargo_monto($descuentorecargo_monto)
-    {
-      $this->descuentorecargo_monto = $descuentorecargo_monto;
-      return $this;
+    public function setDescuentorecargo_monto($descuentorecargo_monto) {
+        $this->descuentorecargo_monto = $descuentorecargo_monto;
+        return $this;
     }
 
     /**
      * @return float
      */
-    public function getDescuentorecargo_porcentaje()
-    {
-      return $this->descuentorecargo_porcentaje;
+    public function getDescuentorecargo_porcentaje() {
+        return $this->descuentorecargo_porcentaje;
     }
 
     /**
      * @param float $descuentorecargo_porcentaje
      * @return \WSDL\Facto\detalle
      */
-    public function setDescuentorecargo_porcentaje($descuentorecargo_porcentaje)
-    {
-      $this->descuentorecargo_porcentaje = $descuentorecargo_porcentaje;
-      return $this;
+    public function setDescuentorecargo_porcentaje($descuentorecargo_porcentaje) {
+        $this->descuentorecargo_porcentaje = $descuentorecargo_porcentaje;
+        return $this;
     }
+    
+    function getImpuesto_codigo() {
+        return $this->impuesto_codigo;
+    }
+
+    function getImpuesto_valorunitario() {
+        return $this->impuesto_valorunitario;
+    }
+
+    function setImpuesto_codigo($impuesto_codigo) {
+        $this->impuesto_codigo = $impuesto_codigo;
+    }
+
+    function setImpuesto_valorunitario($impuesto_valorunitario) {
+        $this->impuesto_valorunitario = $impuesto_valorunitario;
+    }
+
+
 
 }
